392d5451df00b0de912e002a100a984af261a72e
veymarmc <veymar.montano@gmail.com>
2024-07-30 09:51:32 -0400

    begin the implementation
    - delete the default styles
    - make a basic html structure of the main gallery

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react';
 import App from './App';
-import './styles.css';
 import { createRoot } from 'react-dom/client';
 
 document.body.innerHTML = '<div id="app"></div>';
