524d103a78a33f3bcf5e824d5404ae1eb2b5d923
veymarmc <veymar.montano@gmail.com>
2024-08-06 04:50:58 -0400

    implement basic routes in the application
    - install react-router-dom
    - move the app content to a Home component
    - implement the Routes configuration in the App component
    - make a persisten navigation component that will be the element for the
      root part of the routes, so that this will be the main layout part of
      our application

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import App from './App';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.scss';
 
@@ -10,6 +11,8 @@ const root = createRoot(document.getElementById('app'));
 
 root.render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );
