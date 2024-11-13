b19217803fd776afff26d109ed9e409c3d236ec0
veymarmc <veymar.montano@gmail.com>
2024-11-13 09:39:41 -0400

    implement react hooks linter in the application

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,19 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
-  { files: ['src/**/*.{js,mjs,cjs,jsx}'] },
+  {
+    files: ['src/**/*.{js,mjs,cjs,jsx}'],
+    plugins: {
+      'react-hooks': pluginReactHooks,
+    },
+    rules: {
+      ...pluginReactHooks.configs.recommended.rules,
+    },
+  },
   {
     languageOptions: {
       globals: {
@@ -23,6 +32,7 @@ export default [
       'react/jsx-uses-react': 'off',
       'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'off',
+      'react-hooks/exhaustive-deps': 'error',
     },
   },
 ];
